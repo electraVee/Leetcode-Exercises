8e31fe29f8c7c83754a4053c65d82bfbeb64d1c6
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-02 12:24:14 -0400

    Time: 3 ms (45.45%), Space: 8.5 MB (91.94%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
     if(x < 0)
         return 0;
 
-    long int aux = x, inverted = 0;
+    long int aux = x; long int inverted = 0;
 
     while(aux != 0){
         inverted = inverted * 10 + aux % 10;
