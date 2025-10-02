9c96e67da03a2f3b16264ef6ecc831c83050bf12
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-02 12:54:58 -0400

    Time: 0 ms (100%), Space: 29.7 MB (37.56%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
     
-        k %= nums.size(); // ahorra iteraciones normalizando
+    k %= nums.size(); // ahorra iteraciones normalizando con el %
 
  std :: rotate(nums.begin(), nums.end() - k, nums.end()); 
     //rotate es una función de <algorithm> 
