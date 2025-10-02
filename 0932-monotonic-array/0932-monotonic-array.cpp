9204a67a560b8419287c8581d5a1de091e21f7ac
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-02 14:01:47 -0400

    Time: 0 ms (100%), Space: 100.4 MB (64.59%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        bool increasing = true, decreasing = true; //banderines
+        bool increasing = true, decreasing = true;
         
         for(int i = 1; i < nums.size(); i++){
 
             if(nums[i] > nums[i - 1])
-                decreasing = false;
+                decreasing = false;  // desactiva decreciente
 
             if(nums[i] < nums[i - 1])
-                increasing = false;
+                increasing = false; //desactiva creciente
         }
 
-        return increasing || decreasing;
+        return increasing || decreasing; // si alguna sigue activa es true
     }
 };
