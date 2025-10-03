7cb8e255ca87b57a2999063bf63cac1d89da076f
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-02 21:12:15 -0400

    Time: 0 ms (100%), Space: 21.9 MB (38.46%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int summa = 0;
+       int n = nums.size();
+
+       for(int i : nums)
+       summa+= i;
+
+       return (n*(n+1))/2 - summa; // fórmula de sumatoria n * (n + 1) / 2
+
+    }
+};
