c0d6c2a0bde19d313b95e2e6064e043936d28113
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-06 17:25:32 -0400

    Time: 0 ms (100%), Space: 11.2 MB (79.97%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        vector <string> array(n);
+
+        for(int i = 0; i < n; i++){
+            if((i + 1) % 3 == 0 && (i + 1) % 5 == 0){
+                array[i] = "FizzBuzz";
+
+            } else if((i + 1) % 3 == 0){
+                array[i] = "Fizz";
+
+            } else if((i + 1) % 5 == 0){
+                array[i] = "Buzz";
+
+            } else {
+                array[i] = to_string(i + 1);
+            }
+
+        }
+
+        return array;
+    }
+};
