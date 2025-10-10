bac0620d6a91dc68d1d995421ff9d709aa29fb7d
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-10 17:22:21 -0400

    Time: 0 ms (100%), Space: 22.7 MB (19.62%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       vector <int> :: iterator it = unique(nums.begin(),nums.end());
+
+        nums.erase(it,nums.end());
+
+        return nums.size();
+    }
+};
