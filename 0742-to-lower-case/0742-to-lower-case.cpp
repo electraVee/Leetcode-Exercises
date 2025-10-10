f6eab00eb40225b6f9b76b494a284a01087b644f
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-10 15:29:43 -0400

    Time: 0 ms (100%), Space: 8.3 MB (51.75%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string &s) {
+        for(char &c: s){
+            if(c >= 65 && c <= 90)
+                c = c + 32;
+        }
+        return s;
+    }
+};
