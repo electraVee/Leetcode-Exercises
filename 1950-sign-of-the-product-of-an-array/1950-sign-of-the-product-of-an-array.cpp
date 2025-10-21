24ed787d9da7fe32d545a0f31e27a0433911d15b
Verónica Quintero ¡! <veronica.quindu@gmail.com>
2025-10-20 23:48:25 -0400

    Time: 0 ms (100%), Space: 13.8 MB (47.95%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negative = 0;
+
+        for(int i: nums){
+            if(i == 0)
+            return 0;
+
+            if(i < 0)
+            negative++;
+        }
+
+        if(negative % 2 == 0)
+            return 1;
+        
+        return -1;
+    }
+};
