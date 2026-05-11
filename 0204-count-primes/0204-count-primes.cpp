b91dde97f64262de1f171667285cbff234c465f9
Verónica Quintero ¡! <212081535+electraVee@users.noreply.github.com>
2026-05-10 23:43:44 -0400

    Time: 256 ms (11.27%), Space: 12.7 MB (62.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,19 +6,21 @@ public:
             return 0;
         
         vector <bool> primos(n,true);
-
+    long int count = n-2;
         primos[0] = false; primos[1]=false;
 
         for(int i = 2; i < n; i++){
-            if(primos[i] && i <= sqrt(n)){
-                
+            if(primos[i] && i < sqrt(n)){
             for(int j = i*i; j < n; j+=i){
-                primos[j] = false;
+                if (primos[j]) { 
+                    primos[j] = false;
+                    count--;
+                }
             }
 
             }
 
         }
-         return count(primos.begin(), primos.end(),true);
+         return count;
     }
 };
